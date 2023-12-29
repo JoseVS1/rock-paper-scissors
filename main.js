3284137b1ac154084cae6f8f70d7a0935e45cff2
JoseVS1 <105169283+JoseVS1@users.noreply.github.com>
2023-12-29 15:55:12 -0600

    Add basic DOM rendering

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+const selections = document.querySelectorAll(".selection");
+const result = document.querySelector(".result");
+const h1 = document.querySelector("h1");
+const round = document.querySelector("h2");
+const score = document.querySelector("h3");
+
+selections.forEach(button => {
+    console.log(button.textContent)
+    button.addEventListener("click", () => playRound(button.textContent, getComputerChoice()))
+})
+
 function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3);
 
@@ -18,35 +29,35 @@ function playRound(playerSelection, computerSelection) {
     console.log(`You: ${player.charAt(0).toUpperCase() + player.slice(1)}\nComputer: ${computer.charAt(0).toUpperCase() + computer.slice(1)}`);
 
     if (player === computer) {
-        console.log("It's a tie! Let's play again.");
-        playRound(prompt("Make your choice"), getComputerChoice());
+        h1.textContent = "It's a tie! Let's play again.";
+        // playRound(prompt("Make your choice"), getComputerChoice());
     } else if (player === "rock" && computer === "scissors" || player === "paper" && computer === "rock" || player === "scissors" && computer === "paper") {
-        console.log("You win!");
+        h1.textContent = "You win!";
         return "You win!";
     } else {
-        console.log("Computer wins!");
+        h1.textContent = "Computer wins!";
         return "Computer wins!";
     }
 }
 
-function game() {
-    let userScore = 0;
-    let computerScore = 0;
+// function game() {
+//     let userScore = 0;
+//     let computerScore = 0;
 
-    for (let i = 0; i < 5; i++) {
-        console.log(`Round ${i + 1}`);
-        console.log(`Your score: ${userScore}\nComputer score: ${computerScore}`);
+//     for (let i = 0; i < 5; i++) {
+//         round.textContent = `Round ${i + 1}`
+//         score.textContent = `Your score: ${userScore}\nComputer score: ${computerScore}`;
 
-        const result = playRound(prompt("Make your choice: Rock, Scissors or Paper"), getComputerChoice());
-        console.log(`RESULT HAS: ${result}`)
-        if (result === "You win!") {
-            userScore += 1;
-        } else if (result === "Computer wins!") {
-            computerScore += 1;
-        }
-    }
+//         const result = playRound(prompt("Make your choice: Rock, Scissors or Paper"), getComputerChoice());
 
-    console.log(userScore > computerScore ? "You won the game!" : "The computer won the game.");
-}
+//         if (result === "You win!") {
+//             userScore += 1;
+//         } else if (result === "Computer wins!") {
+//             computerScore += 1;
+//         }
+//     }
 
-game();
+//     h1.textContent = userScore > computerScore ? "You won the game!" : "The computer won the game.";
+// }
+
+// game();
